a94f487fe60dae54ae24c48e2fbaca98268b56aa
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-22 13:30:24 +0400

    Post for message added

--- src/main/java/com/example/switterv1/Main.java
+++ src/main/java/com/example/switterv1/Main.java
@@ -5,6 +5,8 @@ import com.example.switterv1.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -24,6 +26,17 @@ public class Main {
 
         Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
         return "main";
     }
 }
