90d154235c2a8e202159fb4582881e04d620585d
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-22 21:35:56 +0400

    Controller added to SQL

---
+++ src/main/java/com/example/switterv1/controller/RegistrationController.java
@@ -0,0 +1,40 @@
+package com.example.switterv1.controller;
+
+import com.example.switterv1.domain.Role;
+import com.example.switterv1.domain.User;
+import com.example.switterv1.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+    @GetMapping("/registration")
+    public String registration() {
+
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if(userFromDb !=  null){
+            model.put("message", "User Exists");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+}
