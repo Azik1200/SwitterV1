9691dbe88a545fa9964ba884760cb3b22741e053
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-22 12:43:15 +0400

    Initial commit

---
+++ src/test/java/com/example/switterv1/SwitterV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.switterv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitterV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
