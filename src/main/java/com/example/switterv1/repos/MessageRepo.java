ec6f9fc6b67237e0f0e9662f4f9e94839c726d25
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-22 13:32:50 +0400

    Filter for message added

--- src/main/java/com/example/switterv1/repos/MessageRepo.java
+++ src/main/java/com/example/switterv1/repos/MessageRepo.java
@@ -4,4 +4,5 @@ import com.example.switterv1.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+    Iterable<Message> findByTag(String filter);
 }
