2c5b40700ac78a1014d58998ecb089e270664b39
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-23 12:47:59 +0400

    userEdit logic is created

--- src/main/java/com/example/switterv1/controller/UserController.java
+++ src/main/java/com/example/switterv1/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.example.switterv1.controller;
 
+import com.example.switterv1.domain.User;
 import com.example.switterv1.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -19,5 +20,11 @@ public class UserController {
     {
         model.addAttribute("users", userRepo.findAll());
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("user", user);
+        return "userEdit";
     }
 }
