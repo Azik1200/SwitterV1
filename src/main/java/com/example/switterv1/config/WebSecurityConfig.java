e2f80e725990d13d92e02c839f7f5861c14bec86
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-26 23:14:42 +0400

    Pass Encoder added to WebConfig

--- src/main/java/com/example/switterv1/config/WebSecurityConfig.java
+++ src/main/java/com/example/switterv1/config/WebSecurityConfig.java
@@ -1,22 +1,31 @@
 package com.example.switterv1.config;
 
 import com.example.switterv1.service.UserSevice;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder(8);
+    }
 
+    private final PasswordEncoder passwordEncoder;
     private final UserSevice userSevice;
 
-    public WebSecurityConfig(UserSevice userSevice) {
+    public WebSecurityConfig(PasswordEncoder passwordEncoder, UserSevice userSevice) {
+        this.passwordEncoder = passwordEncoder;
         this.userSevice = userSevice;
     }
 
@@ -38,6 +47,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userSevice)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(passwordEncoder);
     }
 }
