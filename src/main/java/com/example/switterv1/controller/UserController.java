9863c718c2bc48fa9e4f6446a3a818a2e11d321c
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-26 20:03:21 +0400

    V1.1.0

--- src/main/java/com/example/switterv1/controller/UserController.java
+++ src/main/java/com/example/switterv1/controller/UserController.java
@@ -2,37 +2,35 @@ package com.example.switterv1.controller;
 
 import com.example.switterv1.domain.Role;
 import com.example.switterv1.domain.User;
-import com.example.switterv1.repos.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.switterv1.service.UserSevice;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
 @PreAuthorize("hasAnyAuthority('ADMIN')")
 public class UserController {
 
-    private final UserRepo userRepo;
+    private final UserSevice userSevice;
 
-    public UserController(UserRepo userRepo) {
-        this.userRepo = userRepo;
+    public UserController(UserSevice userSevice) {
+        this.userSevice = userSevice;
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping
     public ModelAndView userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userSevice.findAll());
         return new ModelAndView("userList");
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("{user}")
     public ModelAndView userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -40,32 +38,30 @@ public class UserController {
         return new ModelAndView("userEdit");
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping
     public ModelAndView userSave(@RequestParam("userID") User user,
-                           @RequestParam Map<String, String> form,
-                           @RequestParam String username) {
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepo.save(user);
+                                 @RequestParam Map<String, String> form,
+                                 @RequestParam String username) {
+        userSevice.saveUser(user, username, form);
 
         return new ModelAndView("redirect:/user");
     }
 
-    @GetMapping("/test")
-    @ResponseBody
-    public List<User> test (@RequestParam(name = "email") String email) {
-        return userRepo.findUserCustom(email);
+    @GetMapping("/profile")
+    public ModelAndView getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        return new ModelAndView("profile");
+    }
+
+    @PostMapping("/profile")
+    public ModelAndView updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String password,
+            @RequestParam String email) {
+        userSevice.updateProfile(user, password, email);
+
+        return new ModelAndView("redirect:/user/profile");
     }
 }
