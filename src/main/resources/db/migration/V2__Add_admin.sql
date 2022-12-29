36372c0ac1fe94a06dc861b7edd7afca0cee06c4
Aziz Salimli <azizsalimoglu02@gmail.com>
2022-12-29 12:19:18 +0400

    ShitCode in V2 migration, should be fixed

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
 INSERT INTO usr (id, username, password, active, email, activation_code)
-VALUES (1, 'admin', 'root', b'1', 'admin@admin.com', '');
+VALUES (1, 'admin', '$2a$12$dc2wloXn6JqhJssSqVE1EOQ6/L8icR7C95PINVv4ANv/Q.cjCtxQ.', b'1', 'admin@admin.com', '');
 
 INSERT INTO  user_role (user_id, roles)
 values (1, 'USER'), (1, 'ADMIN');
