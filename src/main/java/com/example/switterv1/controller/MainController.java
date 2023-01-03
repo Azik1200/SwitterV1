61d32b74c98f974e2aa11ce0f4baa182c963b35c
Aziz Salimli <azizsalimoglu02@gmail.com>
2023-01-03 20:23:50 +0400

    ControllerUtils added

--- src/main/java/com/example/switterv1/controller/MainController.java
+++ src/main/java/com/example/switterv1/controller/MainController.java
@@ -3,21 +3,27 @@ package com.example.switterv1.controller;
 import com.example.switterv1.domain.Message;
 import com.example.switterv1.domain.User;
 import com.example.switterv1.repos.MessageRepo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static com.example.switterv1.controller.ControllerUtils.getStringStringMap;
 
 @Controller
 public class MainController {
@@ -30,6 +36,7 @@ public class MainController {
     public MainController(MessageRepo messageRepo) {
         this.messageRepo = messageRepo;
     }
+
     //TODO Смотри сюда и меняй так же.
     @GetMapping
     public ModelAndView greeting(Map<String, Object> model) {
@@ -49,41 +56,49 @@ public class MainController {
         model.addAttribute("messages", messages);
         model.addAttribute("filter", filter);
 
-        return new ModelAndView( "main");
+        return new ModelAndView("main");
     }
 
     @PostMapping("/main")
     public ModelAndView add(
             @AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag, Map<String, Object> model,
+            @Valid Message message,
+            BindingResult bindingResult,
+            Model model,
             @RequestParam("file") MultipartFile file
     ) throws IOException {
-        Message message = new Message(text, tag, user);
+        message.setAuthor(user);
 
-        if (file != null && !file.getOriginalFilename().isEmpty()) {
-            File uploadDir = new File(uploadPath);
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorMap = getStringStringMap(bindingResult);
+            model.mergeAttributes(errorMap);
+        } else {
+            if (file != null && !file.getOriginalFilename().isEmpty()) {
+                File uploadDir = new File(uploadPath);
 
-            if (!uploadDir.exists()) {
-                uploadDir.mkdir();
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+
+                String uuidFile = UUID.randomUUID().toString();
+                String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+                file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+                message.setFilename(resultFilename);
             }
 
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile + "." + file.getOriginalFilename();
 
-            file.transferTo(new File(uploadPath + "/" + resultFilename));
-
-            message.setFilename(resultFilename);
+            messageRepo.save(message);
         }
-
-        messageRepo.save(message);
-
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        model.addAttribute("messages", messages);
 
         return new ModelAndView("main");
     }
+
+
 
     @PostMapping("/filter")
     public ModelAndView filter(@RequestParam String filter, Map<String, Object> model) {
